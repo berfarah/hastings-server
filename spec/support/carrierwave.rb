7ecb654756ea0a827588fa1fcd35dc812ab5b54d
Bernardo Farah <ber@bernardo.me>
2015-11-04 14:23:09 -0800

    Moved a test-related initializer into support
    Had no business being an initializer

---
+++ spec/support/carrierwave.rb
@@ -0,0 +1,14 @@
+ScriptUploader
+
+CarrierWave::Uploader::Base.descendants.each do |klass|
+  next if klass.anonymous?
+  klass.class_eval do
+    def cache_dir
+      "#{Rails.root}/spec/support/uploads/tmp"
+    end
+
+    def store_dir
+      "#{Rails.root}/spec/support/uploads/script/#{model.id}"
+    end
+  end
+end
