c2611052de2a0237966cccd464e3960600b43405
Bernardo Farah <ber@bernardo.me>
2015-11-04 11:44:44 -0800

    Elasticsearch handles scopes faster

--- app/models/log.rb
+++ app/models/log.rb
@@ -10,18 +10,6 @@ class Log < ActiveRecord::Base
   validates :severity, :message, presence: true
   validates_inclusion_of :severity, in: SEVERITY
 
-
-  scope :by_task, -> (task_id) {
-    order(created_at: :desc).includes(:instance)
-      .where(instances: { task_id: task_id })
-  }
-  scope :date, -> (date) {
-    where(created_at: date.beginning_of_day..date.end_of_day)
-  }
-  scope :range, -> (from, to) {
-    where(created_at: from.beginning_of_day..to.end_of_day)
-  }
-
   delegate :name, to: :loggable
   def at
     created_at
--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -9,39 +9,4 @@ RSpec.describe Log, type: :model do
     expect(build(:log, severity: "falsey")).not_to be_valid
     expect(build(:log)).to be_valid
   end
-
-  context "scopes" do
-    before do
-      @yesterday = create(:log)
-      @yesterday.created_at = Time.zone.yesterday
-      @yesterday.save!
-      @today     = create(:log)
-      @tomorrow  = create(:log)
-      @tomorrow.created_at = Time.zone.tomorrow
-      @tomorrow.save!
-    end
-
-    after do
-      [@yesterday, @today, @tomorrow].each(&:destroy!)
-    end
-
-    describe ".date" do
-      subject { described_class.date(Time.zone.today) }
-      it { is_expected.to eq([@today]) }
-    end
-
-    describe ".range" do
-      subject { described_class.range(Time.zone.yesterday, Time.zone.today) }
-      it { is_expected.to include(@yesterday) }
-      it { is_expected.to include(@today) }
-    end
-
-    describe ".by_task" do
-      before { @task = create(:task_with_instances) }
-      after { @task.destroy! }
-      subject { described_class.by_task(@task.id) }
-      it { is_expected.to include(@task.instances.first.logs.first) }
-      it { is_expected.not_to include(@yesterday) }
-    end
-  end
 end
