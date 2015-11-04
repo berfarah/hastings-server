b8147052a704fed7e5727abb766d20f5ae3e09bd
Bernardo Farah <ber@bernardo.me>
2015-11-04 13:55:48 -0800

    Moving RailsHelper dependencies into support
    Makes it more modular/easier to change in the future

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'simplecov'
-require 'chewy/rspec'
+require "shoulda/matchers"
 SimpleCov.start 'rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -46,33 +46,16 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    Chewy.strategy(:bypass)
-  end
-
-  config.after(:each) do
-    FileUtils.rm_rf(Dir[Rails.root.join("spec", "support", "uploads")])
-  end
 
   config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
+    # This option will default to `true` in RSpec 4.
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    # Prevents you from mocking or stubbing a method that does not exist on
-    # a real object. This is generally recommended, and will default to
-    # `true` in RSpec 4.
+    # Prevents you from mocking or stubbing a method that does not exist
     mocks.verify_partial_doubles = true
   end
 
---
+++ spec/support/uploads.rb
@@ -0,0 +1,5 @@
+RSpec.configure do |config|
+  config.after(:each) do
+    FileUtils.rm_rf(Dir[Rails.root.join("spec", "support", "uploads")])
+  end
+end
